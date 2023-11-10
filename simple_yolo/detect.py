0a3910f449628887fca4fce619a3979431bd6353
hannah-OH <hufsohn2014@naver.com>
2023-11-10 09:53:33 +0900

    병합

--- simple_yolo/detect.py
+++ simple_yolo/detect.py
@@ -5,6 +5,7 @@ import platform
 import sys
 from pathlib import Path
 
+
 import torch
 all_labels = []
 
@@ -66,6 +67,8 @@ def run(
     # Directories
     save_dir = Path(project)/'exp'
     save_dir.mkdir(parents=True, exist_ok=True)
+    # save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+    # (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
     # Load model
     device = select_device(device)
@@ -104,6 +107,9 @@ def run(
         # NMS
         with dt[2]:
             pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+
+        # Second-stage classifier (optional)
+        # pred = utils.general.apply_classifier(pred, classifier_model, im, im0s)
 
         # Define the path for the CSV file
         csv_path = save_dir / 'predictions.csv'
@@ -195,6 +201,14 @@ def run(
                         vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer[i].write(im0)
 
+        # Print time (inference-only)
+        # LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+
+    # Print results
+    # t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
+    # LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
+    # 라벨 출력
+    # LOGGER.info(f'label:{all_labels}')
 
     if save_txt or save_img:
         s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
@@ -205,8 +219,8 @@ def run(
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
-    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'best.pt', help='model path or triton URL')
+    parser.add_argument('--source', type=str, default=ROOT / '../web/images/1.png', help='file/dir/URL/glob/screen/0(webcam)')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
--- web/send.py
+++ web/send.py
@@ -1,11 +1,18 @@
 # send.py
 import requests
-
+import subprocess
+import ast
 server_url = "http://127.0.0.1:5000/send-direction"
 
-while True:
-    direction = input('Enter direction (up, down, left, right): ')
-    number = input('Enter number (1, 2, 3): ')
+
+while True:     
+    subprocess.run('python ../simple_yolo/camera.py')
+    result = subprocess.run('python ../simple_yolo/detect.py --weights ../simple_yolo/best.pt --source ./images --hide-conf',
+                            stdout=subprocess.PIPE,
+                            text=True)
+    output_list = ast.literal_eval(result.stdout.strip())
+    direction = output_list[1]
+    number = output_list[0]
     try:
         response = requests.post(server_url, json={'direction': direction, 'number': number})
         if response.status_code == 200:
@@ -14,3 +21,18 @@ while True:
             print(f'Failed to send direction: {response.status_code}')
     except requests.exceptions.RequestException as e:
         print(f'HTTP Request failed: {e}')
+
+
+# 카메라 없을때 테스트 용 
+# server_url = "http://127.0.0.1:5000/send-direction"
+# while True:
+#     direction = input('Enter direction (up, down, left, right): ')
+#     number = input('Enter number (1, 2, 3): ')
+#     try:
+#         response = requests.post(server_url, json={'direction': direction, 'number': number})
+#         if response.status_code == 200:
+#             print('successfully!')
+#         else:
+#             print(f'Failed to send direction: {response.status_code}')
+#     except requests.exceptions.RequestException as e:
+#         print(f'HTTP Request failed: {e}')
